40d08490f6703670d3eaa4de911db885e8463258
Joshua Nichols <treehousejosh@gmail.com>
2026-04-19 15:46:13 -0400

    add gitignore

--- target/debug/deps/rust_vision-21514d79561ca5b3.d
+++
@@ -1,5 +0,0 @@
-/Users/joshuanichols/Documents/repositories/rust-vision/target/debug/deps/rust_vision-21514d79561ca5b3.d: src/main.rs
-
-/Users/joshuanichols/Documents/repositories/rust-vision/target/debug/deps/librust_vision-21514d79561ca5b3.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rust_vision-91a5370d577c9b1f.d
+++
@@ -1,5 +0,0 @@
-/Users/joshuanichols/Documents/repositories/rust-vision/target/debug/deps/rust_vision-91a5370d577c9b1f.d: src/main.rs
-
-/Users/joshuanichols/Documents/repositories/rust-vision/target/debug/deps/librust_vision-91a5370d577c9b1f.rmeta: src/main.rs
-
-src/main.rs:
